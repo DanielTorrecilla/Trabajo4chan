6a305327764b3a4069709e79504c9e0852f8f5db
Rincon009 <125300880+Rincon009@users.noreply.github.com>
2024-02-12 16:03:43 +0100

    Update scriptsMauricio.js

---
+++ scriptsMauricio.js
@@ -0,0 +1,44 @@
+let currentBoardIndex = 0; // Variable global para realizar seguimiento del índice actual del tablero
+
+// Función para mostrar los detalles de un tablero
+function MostrarDetallesForo(index) {
+    currentBoardIndex = index; // Actualiza el índice actual del tablero
+    const board = boards[index]; // Obtiene los datos del tablero en la posición 'index'
+    const modalDetails = document.getElementById('modalDetails'); // Obtiene el elemento del modal donde se mostrarán los detalles
+
+    // Actualiza el contenido del modal con los detalles del tablero
+    modalDetails.innerHTML = `
+        <h2>${board.title} - /${board.board}/</h2>
+        <p>Descripción: ${board.meta_description}</p>
+        <p>Directorio: /${board.board}/</p>
+        <p>Hilos por página: ${board.per_page}</p>
+        <p>Cantidad de páginas: ${board.pages}</p>
+        <p>Tamaño máximo de archivo: ${board.max_filesize} bytes</p>
+        <p>Tamaño máximo de archivo WebM: ${board.max_webm_filesize} bytes</p>
+        <p>Máximo de caracteres en comentarios: ${board.max_comment_chars}</p>
+        <p>Duración máxima de WebM: ${board.max_webm_duration} segundos</p>
+        <p>Límite de bumps: ${board.bump_limit}</p>
+        <p>Límite de imágenes: ${board.image_limit}</p>
+        <p>Cooldown de hilos: ${board.cooldowns.threads} segundos</p>
+        <p>Cooldown de respuestas: ${board.cooldowns.replies} segundos</p>
+        <p>Cooldown de imágenes: ${board.cooldowns.images} segundos</p>
+        <p>Archivado: ${board.is_archived ? 'Sí' : 'No'}</p>
+        <p>Spoilers: ${board.ws_board ? 'Sí' : 'No'}</p>
+    `;
+
+    // Configuración para mostrar el modal
+    const modal = document.getElementById('forumDetailsModal');
+    modal.style.display = "block";
+
+    // Configuración para cerrar el modal cuando se hace clic en la 'X'
+    document.querySelector('.close').onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // Configuración para cerrar el modal cuando se hace clic en cualquier parte fuera del modal
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
